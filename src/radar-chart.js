8054194569d28b00b8becbe307fe2699d4894954
azole <azolelai@gmail.com>
2014-07-08 11:48:53 +0800

    fixed: drag out of range

--- src/radar-chart.js
+++ src/radar-chart.js
@@ -20,7 +20,7 @@ var RadarChart = {
       }
     }
 
-    cfg.maxValue = Math.max(cfg.maxValue, d3.max(d.map(function(o){return o.value})));    
+    cfg.maxValue = Math.max(cfg.maxValue, d3.max(d.map(function(o){return o.value}))); 
     var allAxis = (d.map(function(i, j){return i.axis}));
     var total = allAxis.length;    
     var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
@@ -32,17 +32,18 @@ var RadarChart = {
     var tooltip;
 
     drawFrame();
+    var maxValues = [];
     drawAxis();
     var dataValues = [];
     reCalculatePoints();
     
     var areagg = initPolygon();
-    drawPoly();      
+    drawPoly();
 
     drawnode();
 
     // 框
-    function drawFrame(){      
+    function drawFrame(){
       for(var j=0; j<cfg.levels; j++){
         var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
         g.selectAll(".levels").data(allAxis).enter().append("svg:line")
@@ -51,7 +52,6 @@ var RadarChart = {
          .attr("x2", function(d, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
          .attr("y2", function(d, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
          .attr("class", "line").style("stroke", "grey").style("stroke-width", "0.5px").attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");;
-
       }
     }
     
@@ -62,31 +62,37 @@ var RadarChart = {
       axis.append("line")
           .attr("x1", cfg.w/2)
           .attr("y1", cfg.h/2)
-          .attr("x2", function(j, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
-          .attr("y2", function(j, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+          .attr("x2", function(j, i){
+            maxValues[i] = {x:cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total)), y:0};
+            return maxValues[i].x;
+          })
+          .attr("y2", function(j, i){
+            maxValues[i].y = cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));
+            return maxValues[i].y;
+          })
           .attr("class", "line").style("stroke", "grey").style("stroke-width", "1px");
 
       axis.append("text").attr("class", "legend")
-          .text(function(d){return d}).style("font-family", "sans-serif").style("font-size", "10px").attr("transform", function(d, i){return "translate(0, -10)"})
+          .text(function(d){return d}).style("font-family", "sans-serif").style("font-size", "10px").attr("transform", function(d, i){return "translate(0, -10)";})
           .attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-20*Math.sin(i*cfg.radians/total);})
           .attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))+20*Math.cos(i*cfg.radians/total);});
-    }    
+    }
 
     // 根據輸入的資料計算多邊形要畫的點
-    function reCalculatePoints(){    
+    function reCalculatePoints(){
       g.selectAll(".nodes")
-        .data(d, function(j, i){ 
-          dataValues[i] =        
+        .data(d, function(j, i){
+          dataValues[i] =
           [
-            cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
-            cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
-          ];          
+            cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)),
+            cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total)),
+          ];
         });
       dataValues[d[0].length] = dataValues[0];
     }
 
     // 初始化多邊形
-    function initPolygon(){      
+    function initPolygon(){
       return g.selectAll("area").data([dataValues])
                 .enter()
                 .append("polygon")
@@ -96,13 +102,13 @@ var RadarChart = {
                 .on('mouseover', function (d){
                   z = "polygon."+d3.select(this).attr("class");
                   g.selectAll("polygon").transition(200).style("fill-opacity", 0.1); 
-                  g.selectAll(z).transition(200).style("fill-opacity", .7);
+                  g.selectAll(z).transition(200).style("fill-opacity", 0.7);
                 })
                 .on('mouseout', function(){
                   g.selectAll("polygon").transition(200).style("fill-opacity", cfg.opacityArea);
                 })
-                .style("fill", function(j, i){return cfg.color(0)})
-                .style("fill-opacity", cfg.opacityArea);    
+                .style("fill", function(j, i){return cfg.color(0);})
+                .style("fill-opacity", cfg.opacityArea);
     }
 
     // 畫多邊形
@@ -122,22 +128,22 @@ var RadarChart = {
         .data(d).enter()
         .append("svg:circle").attr("class", "radar-chart-serie0")
         .attr('r', cfg.radius)
-        .attr("alt", function(j){return Math.max(j.value, 0)})
+        .attr("alt", function(j){return Math.max(j.value, 0);})
         .attr("cx", function(j, i){
           return cfg.w/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total));
         })
         .attr("cy", function(j, i){
           return cfg.h/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total));
         })
-        .attr("data-id", function(j){return j.axis})
-        .style("fill", cfg.color(0)).style("fill-opacity", .9)
+        .attr("data-id", function(j){return j.axis;})
+        .style("fill", cfg.color(0)).style("fill-opacity", 0.9)
         .on('mouseover', function (d){
                     newX =  parseFloat(d3.select(this).attr('cx')) - 10;
                     newY =  parseFloat(d3.select(this).attr('cy')) - 5;
                     tooltip.attr('x', newX).attr('y', newY).text(d.value).transition(200).style('opacity', 1);
                     z = "polygon."+d3.select(this).attr("class");
-                    g.selectAll("polygon").transition(200).style("fill-opacity", 0.1); 
-                    g.selectAll(z).transition(200).style("fill-opacity", .7);
+                    g.selectAll("polygon").transition(200).style("fill-opacity", 0.1);
+                    g.selectAll(z).transition(200).style("fill-opacity", 0.7);
                   })
         .on('mouseout', function(){
                     tooltip.transition(200).style('opacity', 0);
@@ -152,7 +158,7 @@ var RadarChart = {
     tooltip = g.append('text').style('opacity', 0).style('font-family', 'sans-serif').style('font-size', 13);
 
 
-    function move(){
+    function move(dobj, i){
       this.parentNode.appendChild(this);
       var dragTarget = d3.select(this);
 
@@ -161,20 +167,28 @@ var RadarChart = {
       var oldX = parseFloat(dragTarget.attr("cx")) - 300;
       var oldY = 300 - parseFloat(dragTarget.attr("cy"));
       var newY = 0, newX = 0, newValue = 0;
+      var maxX = maxValues[i].x - 300;
+      var maxY = 300 - maxValues[i].y;
 
       // 斜率為無限大的特殊情況
       if(oldX == 0) {
-        newY = oldY - d3.event.dy;        
-        newValue = (newY/oldY) * oldData.value; 
+        newY = oldY - d3.event.dy;
+        if(Math.abs(newY) > Math.abs(maxY)) {
+          newY = maxY;
+        }
+        newValue = (newY/oldY) * oldData.value;
       }
       else
       {
         var slope = oldY / oldX;   // 斜率       
         newX = d3.event.dx + parseFloat(dragTarget.attr("cx")) - 300;
+        if(Math.abs(newX) > Math.abs(maxX)) {
+          newX = maxX;
+        }
         newY = newX * slope;
 
         var ratio = newX / oldX; // 利用相似三角形等比的概念計算新的值
-        newValue = ratio * oldData.value; 
+        newValue = ratio * oldData.value;
       }
       
       // 重新設定點的座標
@@ -182,7 +196,7 @@ var RadarChart = {
           .attr("cx", function(){return newX + 300 ;})
           .attr("cy", function(){return 300 - newY;});
       // 重新設定點的值
-      d[oldData.order].value=newValue;  
+      d[oldData.order].value=newValue;
       // 重新計算多邊形的轉折點
       reCalculatePoints();
       // 重畫多邊形
